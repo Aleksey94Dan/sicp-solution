267485de27e46d4213ac293c92c8e7a5857d7e61
Aleksey94Dan <danilchenko.aleksey94@gmail.com>
2020-05-06 11:15:02 +0700

    add 1.23

--- chapter1/1.23.rkt
+++ chapter1/1.23.rkt
@@ -1,5 +1,8 @@
 #lang sicp
 
+(define (next arg)
+  (if (= arg 2) 3
+      (+ arg 2)))
 
 
 (define (smallest-devisor n)
@@ -8,7 +11,7 @@
 (define (find-divisor n test-divisor)
   (cond ((> (square test-divisor) n) n)
         ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (+ test-divisor 1)))))
+        (else (find-divisor n (next test-divisor)))))
 
 (define (divides? a b)
   (= (remainder b a) 0))
@@ -43,22 +46,53 @@
               (search-for-primes (+ start-number 1) (- end-number 1)))
           (search-for-primes (+ start-number 1) end-number))))
 
+
+
+;без процедуры next
 ;(search-for-primes 1000 3)
 ;1009 *** 35
 ;1013 *** 4
 ;1019 *** 4
 
+;без процедуры next
 ;(search-for-primes 10000 3)
 ;10007 *** 34
 ;10009 *** 10
 ;10037 *** 9
 
+;без процедуры next
 ; (search-for-primes 100000 3)
 ; 100003 *** 70
 ; 100019 *** 28
 ; 100043 *** 28
 
+;без процедуры next
 ; (search-for-primes 1000000 3)
 ; 1000003 *** 55
 ; 1000033 *** 27
 ; 1000037 *** 18
+
+
+;с процедурой next
+; (search-for-primes 1000 3)
+; 1009 *** 2
+; 1013 *** 0
+; 1019 *** 1
+
+;с процедурой next
+; (search-for-primes 10000 3)
+; 10007 *** 3
+; 10009 *** 1
+; 10037 *** 1
+
+;с процедурой next
+; (search-for-primes 100000 3)
+; 100003 *** 5
+; 100019 *** 3
+; 100043 *** 3
+
+;с процедурой next
+; (search-for-primes 1000000 3)
+; 1000003 *** 22
+; 1000033 *** 8
+; 1000037 *** 7
