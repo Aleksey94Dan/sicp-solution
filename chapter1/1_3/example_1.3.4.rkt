448c15fb9a33a2c0a3909255903cc73e1e52d921
Aleksey94Dan <50262563+Aleksey94Dan@users.noreply.github.com>
2020-06-28 14:18:59 +0700

    Update example_1.3.4.rkt

--- chapter1/1_3/example_1.3.4.rkt
+++ chapter1/1_3/example_1.3.4.rkt
@@ -34,3 +34,45 @@
 
 (sqrt 2)
 (cube-root 3)
+
+(define (square x) (* x x))
+(define (deriv g)
+  (define dx 0.00001)
+  (lambda (x)
+  (/ (- (g (+ x dx)) (g x))
+    dx)))
+
+(define (cube x) (* x x x))
+
+((deriv cube) 5)
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((deriv g) x)))
+    )
+)
+
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (sqrt x)
+  (newtons-method
+  (lambda (y) (- (square y) x)) 1.0
+  )
+)
+
+(sqrt 2)
+(define (fixed-point-of-transform g transform guess)
+  (fixed-point (transform g) guess))
+
+(define (sqrt x)
+  (fixed-point-of-transform (lambda (y) (- (square y) x))
+    newton-transform
+    1.0))
+
+(define (sqrt x)
+  (fixed-point-of-transform (lambda (y) (- (square y) x))
+    average-damp
+    1.0))
+
