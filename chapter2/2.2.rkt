069ff3d4b744499bd513a87abe6ba5a5c5f40ff6
Aleksey Danilchenko <danilchenko.aleksey94@gmail.com>
2020-09-01 23:46:02 +0700

    add 2.3.rkt

--- chapter2/2.2.rkt
+++ chapter2/2.2.rkt
@@ -1,5 +1,6 @@
 #lang racket/base
 
+(require rackunit)
 
 (define (make-segment start end)
   (cons start end))
@@ -22,5 +23,30 @@
 
 (define (y-point point)
   (cdr point))
+
+
+
+(define (midpoint-segment segment)
+  (let ((x1 (x-point (start-segment segment)))
+        (x2 (x-point (end-segment segment)))
+        (y1 (y-point (start-segment segment)))
+        (y2 (y-point (end-segment segment))))
+    (make-point (/ (+ x1 x2) 2) (/ (+ y1 y2) 2))))
+
+
+(define (AB-segment)
+  (make-segment (make-point 1 1) (make-point 5 1)))
+
+
+(define (AC-segment)
+  (make-segment (make-point 2 1) (make-point 8 3)))
+
+(define (0-segment)
+  (make-segment (make-point 0 0) (make-point 0 0)))
+
+
+(check-equal? (midpoint-segment (AB-segment)) (make-point 3 1))
+(check-equal? (midpoint-segment (AC-segment)) (make-point 5 2))
+(check-equal? (midpoint-segment (0-segment)) (make-point 0 0))
 
 
